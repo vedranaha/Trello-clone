9b728092897de1cf37d61c399bcce06f305e6355
Vedranaha <vedranaha@gmail.com>
2021-08-16 18:55:07 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import TodoForm from "./components/Todoform";
+import Todo from "./components/Todo";
 import "./App.css";
 
 function App() {
+  const [listTodos, setListTodos] = useState([]);
+
+  const newTodo = (todo) => {
+    setListTodos([todo, ...listTodos]);
+  };
+
+  const clean = (id) => {
+    const listFiltered = listTodos.filter((e, index) => index !== id);
+    setListTodos(listFiltered);
+  };
+
+  const updateTodo = (id, todo) => {
+    const listUpdated = listTodos.map((e, index) => {
+      if (index === id) {
+        e = todo;
+      }
+
+      return e;
+    });
+
+    setListTodos(listUpdated);
+  };
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <TodoForm newTodo={newTodo} />
+
+      <div className="list">
+        {listTodos.map((e, index) => (
+          <Todo todo={e} clean={clean} id={index} edit={updateTodo} />
+        ))}
+      </div>
     </div>
   );
 }
