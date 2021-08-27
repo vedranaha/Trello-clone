7d2782b7b570d940501cec1d644e240d2d0f4b31
Vedranaha <vedranaha@gmail.com>
2021-08-27 03:06:16 +0200

    droppable.draggable - list

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "./Card";
 import store from "../store";
 import ContainerCardForm from "./ContainerCardForm";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
 export default function List({ list, index, listId }) {
   const [data, setData] = useState(store);
@@ -43,17 +44,39 @@ export default function List({ list, index, listId }) {
   };
 
   return (
-    <div className="list">
-      <h2 className="listTitle">{list.title} </h2>
-      {list.cards.map((card, index) => (
-        <Card key={card.id} card={card} index={index} deleteCard={deleteCard} />
-      ))}
-      <div>
-        <ContainerCardForm listId={list.id} type="card" />
-        <div className="XbtnList" onClick={() => deleteList(listId)}>
-          Clean list
+    <Draggable draggableId={list.id} index={index}>
+      {(provided) => (
+        <div
+          className="list"
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
+          <div {...provided.dragHandleProps}>
+            <h2 className="listTitle">{list.title} </h2>
+            <Droppable droppableId={list.id}>
+              {(provided) => (
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  {list.cards.map((card, index) => (
+                    <Card
+                      key={card.id}
+                      card={card}
+                      index={index}
+                      deleteCard={deleteCard}
+                    />
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+            <div>
+              <ContainerCardForm listId={list.id} type="card" />
+              <div className="XbtnList" onClick={() => deleteList(listId)}>
+                Clean list
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 }
