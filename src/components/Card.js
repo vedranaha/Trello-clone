9cbc0eb7eda2bec46c26e212e653eb8f9d00ad43
Vedranaha <vedranaha@gmail.com>
2021-08-27 02:57:19 +0200

    dragable card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
 export default function Card({ card, index, id, deleteCard }) {
   return (
-    <div className="task">
-      {card.title}
-      <div className="Xbtn" onClick={() => deleteCard(id, index)}>
-        X
-      </div>
-    </div>
+    <Draggable draggableId={card.id} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+        >
+          <div className="task">
+            {card.title}
+            <div className="Xbtn" onClick={() => deleteCard(id, index)}>
+              X
+            </div>
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 }
