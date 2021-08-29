065b04066253ca57b6d055d6411f9f46570a4e87
vedranaha <51958563+vedranaha@users.noreply.github.com>
2021-08-29 23:15:36 +0200

    Update store.js
    added one more list

--- src/store.js
+++ src/store.js
@@ -22,11 +22,17 @@ const data = {
     },
     "list-2": {
       id: "list-2",
+      title: "Doing",
+      cards: [],
+    },
+    "list-3": {
+      id: "list-3",
       title: "Done",
       cards: [],
     },
   },
-  listIds: ["list-1", "list-2"],
+  listIds: ["list-1", "list-2", "list-3"],
 };
+
 
 export default data;
